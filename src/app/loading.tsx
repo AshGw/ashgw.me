3665ec93c7ab6eff46bd7c95ad97172b90dec343
AshGw <AshrefGw@proton.me>
2024-01-15 05:10:28 +0100

    feat: set blog sorting based on time of first publish

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -16,13 +16,3 @@ function LoadingScreen() {
 }
 
 export default LoadingScreen;
-
-function Loading() {
-  return (
-    <div className="loader">
-      <div className="one"></div>
-      <div className="two"></div>
-      <div className="three"></div>
-    </div>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+'use client';
+
 import ThemeModeToggler from './components/reusables/theme-toggler';
+import { Button } from './components/ui/button';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
   return (
     <div className="mx-auto container sm:max-w-xl md:max-w-3xl lg:max-w-3xl xl:max-w-3xl">
-      This is basically the home page
-      <div>
-        <ThemeModeToggler></ThemeModeToggler>
+      <div className="flex items-center justify-center h-screen">
+        <Button
+          onClick={() => {
+            window.location.href = '/blog';
+          }}
+        >
+          Check out my blog here
+        </Button>
+        <div className="px-5">
+          <ThemeModeToggler></ThemeModeToggler>
+        </div>
       </div>
     </div>
   );
