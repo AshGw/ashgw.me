3cb776bd1523c68cea6e7c376e642cafec9ddbad
AshGw <AshrefGw@proton.me>
2024-02-05 08:38:17 +0100

    feat: add `Header`

--- src/app/components/mdx/styled-mdx.tsx
+++ src/app/components/mdx/styled-mdx.tsx
@@ -9,7 +9,13 @@ export default function StyledMDX({ source }: { source: string }) {
   return (
     <_StyledMDX
       source={source}
-      components={{ Code: Code, Nav: Navbar, Image: StyledImage }}
+      components={{
+        Code: Code,
+        Nav: Navbar,
+        Image: StyledImage,
+        H: Heading,
+        S: Spacer,
+      }}
     ></_StyledMDX>
   );
 }
@@ -21,22 +27,28 @@ interface StyledImageProps {
   className?: string;
 }
 
-function StyledImage(p: StyledImageProps) {
+function StyledImage(props: StyledImageProps) {
   return (
-    <Skeleton>
+    <Skeleton className="flex items-center justify-center">
       <Image
         isLoading
         isBlurred
         as={NextImage}
-        className={cn('rounded-lg', p.className || '')}
-        src={p.src}
+        className={cn('rounded-lg', props.className || '')}
+        src={props.src}
         alt="..."
-        height={p.height}
-        width={p.width}
+        height={props.height}
+        width={props.width}
       />
-      ;
     </Skeleton>
   );
+}
+
+export async function Heading({ children }: { children: React.ReactNode }) {
+  return <div className="text-4xl font-bold">{children}</div>;
+}
+export async function Spacer({ children }: { children: React.ReactNode }) {
+  return <div className="my-5">{children}</div>;
 }
 
 export const Code: React.FC<{
