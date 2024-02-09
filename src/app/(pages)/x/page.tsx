d371d1b1900466ebd6c7797548b0a717644fb2e8
AshGw <AshrefGw@proton.me>
2024-02-10 00:24:43 +0100

    refactor: code block & copy componnets into their own dirs

--- src/app/(pages)/x/page.tsx
+++ src/app/(pages)/x/page.tsx
@@ -1,10 +1,10 @@
 import LoadingScreen from '@/app/components/reusables/loading-screen';
 import Nav from '@/app/components/nav/nav';
+import StartUp from '@/app/components/protos/startup';
 export default function Fun() {
   return (
     <>
-      <Nav></Nav>
-      <LoadingScreen></LoadingScreen>
+      <StartUp />
     </>
   );
 }
