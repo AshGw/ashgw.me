1b7e28e3a902b9e9fa4896a6adfb38cdbec315c8
AshGw <AshrefGw@proton.me>
2024-02-12 21:50:09 +0100

    chore: remove `dimmed` from medium section

--- src/app/components/reusables/sections.tsx
+++ src/app/components/reusables/sections.tsx
@@ -1,6 +1,6 @@
 export function MediumSection({ children }: { children: React.ReactNode }) {
   return (
-    <section className="dimmed-5 mx-auto container sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-4xl">
+    <section className="mx-auto container sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-4xl">
       {children}
     </section>
   );
