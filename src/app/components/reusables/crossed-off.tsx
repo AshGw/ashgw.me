b019e26e6b824adedeb6680a568a10833833e59b
AshGw <AshrefGw@proton.me>
2024-04-01 17:17:30 +0100

    fix: some typos in bootcamps ripoff blog

--- src/app/components/reusables/crossed-off.tsx
+++ src/app/components/reusables/crossed-off.tsx
@@ -5,5 +5,9 @@ export default function CrossedOff({
 }: {
   children: React.ReactNode;
 }) {
-  return <s>{children}</s>;
+  return (
+    <span className="line-through bg-gradient-to-r from-gray-500 to-gray-200 text-transparent bg-clip-text">
+      {children}
+    </span>
+  );
 }
