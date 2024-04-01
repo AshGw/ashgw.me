7b7d2e91e5f124b238ecf1cb1e4236c91954e6b7
AshGw <AshrefGw@proton.me>
2024-04-01 22:42:32 +0000

    feat: make `Link` component pick up if inside blog context or not

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,10 @@
+import { nextJS, pub } from './env';
 export const BLOG_CONTENT_PATH = '/public/blogs';
 export const BLOG_URI = '/blog';
 export const BLOG_API_URI = '/api/blogs';
+export const SITE_URL =
+  nextJS.NEXT_NODE_ENV === 'production' ? pub.SITE_URL_PROD : pub.SITE_URL_DEV;
+
 export const Status = {
   OK: 200,
   BAD_REQUEST: 400,
