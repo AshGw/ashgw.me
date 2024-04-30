e17e5eae3f5f4d0e63d2ee179281a149c3039264
AshGw <AshrefGw@proton.me>
2024-04-30 01:52:34 +0100

    chore(#173): always fail safe with `mdx` content retrieval

--- src/lib/mdx/content.ts
+++ src/lib/mdx/content.ts
@@ -1,42 +1,41 @@
 'use server';
-// TODO: refactor MDX stuff into its own file
 
+import type { MaybeUndefined, Optional } from 'ts-roids';
 import { promises as fsPromises } from 'fs';
 import path from 'path';
 import fm from 'front-matter';
 import type { MDXData, PostData } from '@/lib/types/mdx';
 import { BLOG_CONTENT_PATH, BUSINESS_CONTENT_PATH } from '@/lib/constants';
-import type { Maybe } from '@/lib/types/global';
 
 function parseMDX(content: string): MDXData {
   return fm(content) as MDXData;
 }
 
-async function getMDXFiles(dir: string): Promise<string[]> {
+async function getMDXFiles(dir: string): Promise<Optional<string[]>> {
   try {
     const files = await fsPromises.readdir(dir);
     const mdxFiles = files.filter((file) => path.extname(file) === '.mdx');
     return mdxFiles;
   } catch (error) {
-    console.error('Error reading directory:', error);
-    // TODO: handle error
-    throw error;
+    console.error('Error reading MDX files:', error);
+    return null;
   }
 }
 
-async function readMDXFile(filePath: string): Promise<MDXData> {
+async function readMDXFile(filePath: string): Promise<Optional<MDXData>> {
   try {
     const rawContent = await fsPromises.readFile(filePath, 'utf-8');
     return parseMDX(rawContent);
   } catch (error) {
-    // TODO: handle err
     console.error('Error reading MDX file:', error);
-    throw new Error();
+    return null;
   }
 }
-async function getMDXData(dir: string): Promise<PostData[]> {
+async function getMDXData(dir: string): Promise<Optional<PostData[]>> {
   const mdxFiles = await getMDXFiles(dir);
-
+  if (mdxFiles === null) {
+    return null;
+  }
   const blogDataPromises = mdxFiles.map(async (file) => {
     const parsedContent = await readMDXFile(path.join(dir, file));
     const filename: string = path.basename(file, path.extname(file));
@@ -46,31 +45,50 @@ async function getMDXData(dir: string): Promise<PostData[]> {
     };
   });
 
-  return Promise.all(blogDataPromises);
+  const blogData = await Promise.all(blogDataPromises);
+  return blogData as PostData[];
 }
 
 export async function getBlogPosts(
   blogDirectory: string = BLOG_CONTENT_PATH
-): Promise<PostData[]> {
+): Promise<Optional<PostData[]>> {
   return getMDXData(path.join(process.cwd(), blogDirectory));
 }
 
 export async function getBusinessPosts(
   businessDirectory: string = BUSINESS_CONTENT_PATH
-): Promise<PostData[]> {
+): Promise<Optional<PostData[]>> {
   return getMDXData(path.join(process.cwd(), businessDirectory));
 }
 
-export async function getBlogPost(slug: string): Promise<Maybe<PostData>> {
-  // TODO: optimize
+export async function getBlogPost(slug: string): Promise<Optional<PostData>> {
   const blogs = await getBlogPosts();
-  const blogPost = blogs.find((p) => p?.filename === slug);
+  if (blogs === null) {
+    return null;
+  }
+  const blogPost: MaybeUndefined<PostData> = blogs.find(
+    (p) => p?.filename === slug
+  );
+  if (blogPost === undefined) {
+    console.log('No blog post matched the given slug');
+    return null;
+  }
   return blogPost;
 }
 
-export async function getBusinessPost(slug: string): Promise<Maybe<PostData>> {
-  // TODO: optimize
+export async function getBusinessPost(
+  slug: string
+): Promise<Optional<PostData>> {
   const blogs = await getBusinessPosts();
-  const blogPost = blogs.find((p) => p?.filename === slug);
+  if (!blogs) {
+    return null;
+  }
+  const blogPost: MaybeUndefined<PostData> = blogs.find(
+    (p) => p?.filename === slug
+  );
+  if (blogPost === undefined) {
+    console.log('No blog post matched the given slug');
+    return null;
+  }
   return blogPost;
 }
