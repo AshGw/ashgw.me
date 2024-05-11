b61af551dc6b9379c0ae9e34beb283d5119ea363
AshGw <AshrefGw@proton.me>
2024-05-11 02:37:20 +0100

    refactor: remove unecessary unused env vars

--- test/code/env.test.ts
+++ test/code/env.test.ts
@@ -1,11 +1,9 @@
 import { pub, nextJS } from '@/lib/env';
 
-test('Environment variables should actually load', async () => {
+test('Environment variables should exist', async () => {
   // NextJS configs
   expect(nextJS.GENERATE_SOURCEMAP).toBeDefined();
-  expect(nextJS.NEXT_URL).toBeDefined();
   expect(nextJS.NEXT_NODE_ENV).toBeDefined();
-  expect(nextJS.NEXT_DEV_URL).toBeDefined();
 
   // public envs
   expect(pub.SITE_URL_PROD).toBeDefined();
