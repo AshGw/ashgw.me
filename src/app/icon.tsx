505de373e3d28627d875885a7117b9a341af7bb2
AshGw <AshrefGw@proton.me>
2024-05-20 23:08:09 +0100

    build: enable post-build scripts

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,10 +1,10 @@
-import { ImageResponse } from 'next/og'
+import { ImageResponse } from 'next/og';
 
 export const size = {
   width: 32,
   height: 32,
-}
-export const contentType = 'image/png'
+};
+export const contentType = 'image/png';
 
 export default function Icon() {
   return new ImageResponse(
@@ -27,5 +27,5 @@ export default function Icon() {
     {
       ...size,
     }
-  )
+  );
 }
