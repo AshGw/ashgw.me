bb8a48636ee7e7950153e0e826dfee358f2972d1
AshGw <AshrefGw@proton.me>
2024-05-22 04:18:12 +0100

    fix: punctuations and typos 2

--- src/app/components/reusables/code/code-block.tsx
+++ src/app/components/reusables/code/code-block.tsx
@@ -577,6 +577,36 @@ import CopyButton from './copy-code';
 // @ts-nocheck
 
 // react-syntax-highlighter has no types
+
+/* eslint-disable */
+// @ts-nocheck
+
+// react-syntax-highlighter has no types
+
+/* eslint-disable */
+// @ts-nocheck
+
+// react-syntax-highlighter has no types
+
+/* eslint-disable */
+// @ts-nocheck
+
+// react-syntax-highlighter has no types
+
+/* eslint-disable */
+// @ts-nocheck
+
+// react-syntax-highlighter has no types
+
+/* eslint-disable */
+// @ts-nocheck
+
+// react-syntax-highlighter has no types
+
+/* eslint-disable */
+// @ts-nocheck
+
+// react-syntax-highlighter has no types
 /* eslint-disable */
 // @ts-nocheck
 
