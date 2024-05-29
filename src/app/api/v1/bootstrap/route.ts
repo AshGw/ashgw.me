bf30d238cf0f6672bef6a8084955d1ce174404d0
AshGw <AshrefGw@proton.me>
2024-05-29 04:31:01 +0100

    feat: add dotfiles bootstrap endpoint

---
+++ src/app/api/v1/bootstrap/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+import { NewType } from 'ts-roids';
+
+// TODO: KEEP TRACK OF STABLE COMMITS, DON'T JUST FETCH OFF OF MAIN
+const BOOTSTRAP_SCRIPT =
+  'https://raw.githubusercontent.com/AshGw/dotfiles/main/install/bootstrap' as const;
+
+type Script = NewType<'Key', string>;
+type Err = string;
+export async function GET(): Promise<NextResponse<Script> | NextResponse<Err>> {
+  try {
+    const res = await fetch(BOOTSTRAP_SCRIPT, {
+      method: 'GET',
+      mode: 'no-cors',
+      next: { revalidate: 36969 },
+    });
+    if (!res.ok) {
+      return new NextResponse('Failed to fetch the bootstrap script', {
+        status: 424,
+      });
+    }
+
+    const key = (await res.text()) as Script;
+    return new NextResponse(key, { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return new NextResponse('Something unexpected happened', {
+      status: 500,
+    });
+  }
+}
