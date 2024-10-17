5f2fb04eef8361df3114365166b7a0f690a1649a
AshGw <ashrefgw@proton.me>
2024-10-17 22:21:57 +0100

    build: use not acme

---
+++ apps/nextjs/next.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath } from "url";
+import { createJiti } from "jiti";
+
+// Import env files to validate at build time. Use jiti so we can load .ts files in here.
+await createJiti(fileURLToPath(import.meta.url)).import("./src/env");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: [
+    "@ashgw/api",
+    "@ashgw/auth",
+    "@ashgw/db",
+    "@ashgw/ui",
+    "@ashgw/validators",
+  ],
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default config;
---
+++ apps/nextjs/src/trpc/server.ts
@@ -0,0 +1,31 @@
+import { cache } from "react";
+import { headers } from "next/headers";
+import { createHydrationHelpers } from "@trpc/react-query/rsc";
+
+import type { AppRouter } from "@ashgw/api";
+import { createCaller, createTRPCContext } from "@ashgw/api";
+import { auth } from "@ashgw/auth";
+
+import { createQueryClient } from "./query-client";
+
+/**
+ * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
+ * handling a tRPC call from a React Server Component.
+ */
+const createContext = cache(async () => {
+  const heads = new Headers(headers());
+  heads.set("x-trpc-source", "rsc");
+
+  return createTRPCContext({
+    session: await auth(),
+    headers: heads,
+  });
+});
+
+const getQueryClient = cache(createQueryClient);
+const caller = createCaller(createContext);
+
+export const { trpc: api, HydrateClient } = createHydrationHelpers<AppRouter>(
+  caller,
+  getQueryClient,
+);
