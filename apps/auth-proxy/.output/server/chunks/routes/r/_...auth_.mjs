15f8378083a1bcec50476828741d4d94fe7eac98
AshGw <ashrefgw@proton.me>
2024-10-23 11:08:21 +0100

    build: do not track cache

--- apps/auth-proxy/.output/server/chunks/routes/r/_...auth_.mjs
+++ apps/auth-proxy/.output/server/chunks/routes/r/_...auth_.mjs
@@ -1,14 +1,16 @@
-import { Auth } from '@auth/core';
-import Discord from '@auth/core/providers/discord';
-import { e as eventHandler, t as toWebRequest } from '../../runtime.mjs';
-import 'node:http';
-import 'node:https';
-import 'node:fs';
-import 'node:path';
-import 'node:url';
+import { Auth } from "@auth/core";
+import Discord from "@auth/core/providers/discord";
 
-const ____auth_ = eventHandler(
-  async (event) => Auth(toWebRequest(event), {
+import { e as eventHandler, t as toWebRequest } from "../../runtime.mjs";
+
+import "node:http";
+import "node:https";
+import "node:fs";
+import "node:path";
+import "node:url";
+
+const ____auth_ = eventHandler(async (event) =>
+  Auth(toWebRequest(event), {
     basePath: "/r",
     secret: process.env.AUTH_SECRET,
     trustHost: !!process.env.VERCEL,
@@ -16,10 +18,10 @@ const ____auth_ = eventHandler(
     providers: [
       Discord({
         clientId: process.env.AUTH_DISCORD_ID,
-        clientSecret: process.env.AUTH_DISCORD_SECRET
-      })
-    ]
-  })
+        clientSecret: process.env.AUTH_DISCORD_SECRET,
+      }),
+    ],
+  }),
 );
 
 export { ____auth_ as default };
--- apps/auth-proxy/.output/server/index.mjs
+++ apps/auth-proxy/.output/server/index.mjs
@@ -1,7 +1,13 @@
-import process from 'node:process';globalThis._importMeta_={url:import.meta.url,env:process.env};import 'node:http';
-import 'node:https';
-export { n as default } from './chunks/runtime.mjs';
-import 'node:fs';
-import 'node:path';
-import 'node:url';
+import process from "node:process";
+
+import "node:http";
+import "node:https";
+import "node:fs";
+import "node:path";
+import "node:url";
+
+globalThis._importMeta_ = { url: import.meta.url, env: process.env };
+
+export { n as default } from "./chunks/runtime.mjs";
+
 //# sourceMappingURL=index.mjs.map
