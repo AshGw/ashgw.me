4857e342de5d5078f7477ff621878ba2280c3d3f
Ashref Gwader <ashrefgw@proton.me>
2024-11-02 00:34:20 +0100

    fix: mic (#327)

--- src/app/components/reusables/back-up-top.tsx
+++ src/app/components/reusables/back-up-top.tsx
@@ -5,25 +5,49 @@ import { useEffect, useState } from 'react';
 
 export default function BackUpTop() {
   const [isVisible, setIsVisible] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [scrollTimeout, setScrollTimeout] = useState<ReturnType<
+    typeof setTimeout
+  > | null>(null);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 100) {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
         setIsVisible(true);
       } else {
         setIsVisible(false);
       }
+
+      if (scrollTimeout) {
+        clearTimeout(scrollTimeout);
+      }
+
+      setScrollTimeout(
+        setTimeout(() => {
+          setIsVisible(false);
+        }, 1069)
+      );
+
+      setLastScrollY(currentScrollY);
     };
 
     window.addEventListener('scroll', handleScroll);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      if (scrollTimeout) {
+        clearTimeout(scrollTimeout);
+      }
     };
-  }, []);
+  }, [lastScrollY, scrollTimeout]);
 
-  return isVisible ? (
-    <button className="fixed max-w-3 max-h-3 average-transition animate-bounce bottom-1 right-1 mx-12 md:px-18 lg:mx-24 xl:mx-[200px] my-12 hover:opacity-100">
+  return (
+    <button
+      className={`fixed max-w-3 max-h-3 average-transition animate-bounce bottom-1 right-1 mx-12 md:px-18 lg:mx-24 xl:mx-[200px] my-12 
+        ${isVisible ? 'opacity-100 transition-opacity duration-300' : 'opacity-0 transition-opacity duration-300'}`}
+    >
       <ChevronUp
         onClick={() => {
           window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -32,5 +56,5 @@ export default function BackUpTop() {
         className="average-transition hover:opacity-100 hover:scale-150"
       />
     </button>
-  ) : null;
+  );
 }
