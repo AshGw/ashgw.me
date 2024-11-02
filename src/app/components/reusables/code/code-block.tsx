2f83c2d2182b25b896b44f26a490ad27acf758ca
AshGw <ashrefgw@proton.me>
2024-11-02 02:03:45 +0100

    ci: set sync label to true

--- src/app/components/reusables/code/code-block.tsx
+++ src/app/components/reusables/code/code-block.tsx
@@ -702,6 +702,11 @@ import CopyButton from './copy-code';
 // @ts-nocheck
 
 // react-syntax-highlighter has no types
+
+/* eslint-disable */
+// @ts-nocheck
+
+// react-syntax-highlighter has no types
 /* eslint-disable */
 // @ts-nocheck
 SyntaxHighlighter.registerLanguage('rust', rust);
