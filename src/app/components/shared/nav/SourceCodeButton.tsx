ae19e82e987c53d17a21fc7d178248ce550c9b07
AshGw <ashrefgw@proton.me>
2024-11-12 20:33:30 +0100

    add git button

---
+++ src/app/components/shared/nav/SourceCodeButton.tsx
@@ -0,0 +1,23 @@
+import { GitBranch } from 'lucide-react';
+import NextLink from 'next/link';
+
+interface SourceCodeButtonProps {
+  href: string;
+}
+export function SourceCodeButton({ href }: SourceCodeButtonProps) {
+  return (
+    <button>
+      <NextLink href={href} target="_blank" rel="noopener noreferrer">
+        <GitBranch
+          strokeWidth={'1.5px'}
+          size={'20px'}
+          style={{
+            display: 'inline-block',
+            marginLeft: '5px',
+            marginBottom: '3px',
+          }}
+        />
+      </NextLink>{' '}
+    </button>
+  );
+}
