9dd8debd173a9c1f4c95a279065ed36ca921ab56
Ashref Gwader <ashref@ashgw.me>
2024-11-26 21:00:03 +0100

    fix: update booking link (#383)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -27,7 +27,7 @@ export const PUBLIC_CONTENT_SOURCE =
 export const SITE_NAME = getSiteName(pub.SITE_URL_PROD);
 export const REPO_SOURCE = 'https://github.com/ashgw/ashgw.me';
 export const CREATOR = 'Ashref Gwader';
-export const BOOKING_LINK = 'https://cal.com/ashgw';
+export const BOOKING_LINK = 'https://cal.com/ashgw/30min';
 
 export const LINKS = {
   npm: 'https://www.npmjs.com/~ashgw',
