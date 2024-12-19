194fe3db62e9a202e3310290c6c66bab2a7b551b
AshGw <ashrefgw@proton.me>
2024-12-19 16:01:08 +0100

    fix: ui package lint issues

--- packages/ui/src/label.tsx
+++ packages/ui/src/label.tsx
@@ -1,13 +1,14 @@
-'use client';
-import * as LabelPrimitive from '@radix-ui/react-label';
-import { type VariantProps } from 'class-variance-authority';
-import { cva } from 'class-variance-authority';
-import * as React from 'react';
+"use client";
+
+import type { VariantProps } from "class-variance-authority";
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva } from "class-variance-authority";
 
 import { cn } from "@ashgw/ui";
 
 const labelVariants = cva(
-  'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
 );
 
 const Label = React.forwardRef<
